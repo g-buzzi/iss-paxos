a7e8832d2eab7e20963d31ab534ddb4d27b557d7
Gabriel Buzzi <gabriel.buzzi7@gmail.com>
2024-08-25 11:20:25 -0300

    Configs for experiments

--- bin/client_test.sh
+++ bin/client_test.sh
@@ -7,13 +7,13 @@ PID=$(cat "${PID_FILE}");
 if [ -z "${PID}" ]; then
     echo "Process id for clients is written to location: {$PID_FILE}"
     go build ../client/
-    ./client -id 1.1 -algorithm iss -config config.json &
+    ./client -id 1.1 -algorithm iss -config config_3.json &
     echo $! >> ${PID_FILE}
-    ./client -id 1.2 -algorithm iss -config config.json &
+    ./client -id 1.2 -algorithm iss -config config_3.json &
     echo $! >> ${PID_FILE}
-    ./client -id 1.3 -algorithm iss -config config.json &
+    ./client -id 1.3 -algorithm iss -config config_3.json &
     echo $! >> ${PID_FILE}
-    ./client -id 1.4 -algorithm iss -config config.json &
+    ./client -id 1.4 -algorithm iss -config config_3.json &
     echo $! >> ${PID_FILE}
 else
     echo "Clients are already started in this folder."
--- bin/server_test.sh
+++ bin/server_test.sh
@@ -9,13 +9,11 @@ if [ -z "${PID}" ]; then
     go build ../server/
     go build ../client/
     go build ../cmd/
-    ./server -log_dir=. -log_level=debug -id 1.1 -algorithm=wpaxos -heartbeat=100 -batch=0 &
+    ./server -log_dir=. -log_level=debug -id 1.1 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config_3.json &
     echo $! >> ${PID_FILE}
-    ./server -log_dir=. -log_level=debug -id 1.2 -algorithm=wpaxos -heartbeat=100 -batch=0 &
+    ./server -log_dir=. -log_level=debug -id 1.2 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config_3.json &
    echo $! >> ${PID_FILE}
-    ./server -log_dir=. -log_level=debug -id 1.3 -algorithm=wpaxos -heartbeat=100 -batch=0 &
-    echo $! >> ${PID_FILE}
-    ./server -log_dir=. -log_level=debug -id 1.4 -algorithm=wpaxos -heartbeat=100 -batch=0 &
+    ./server -log_dir=. -log_level=debug -id 1.3 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config_3.json &
     echo $! >> ${PID_FILE}
 else
     echo "Servers are already started in this folder."
