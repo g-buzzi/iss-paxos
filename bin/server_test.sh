0169d64b1337ddae8b29b87e10b76e9e16b94e00
Gabriel Buzzi <gabriel.buzzi7@gmail.com>
2024-09-07 19:37:51 -0300

    exp06

--- bin/server_test.sh
+++ bin/server_test.sh
@@ -9,11 +9,11 @@ if [ -z "${PID}" ]; then
     go build ../server/
     go build ../client/
     go build ../cmd/
-    ./server -log_dir=. -log_level=debug -id 1.1 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config_3.json &
+    ./server -log_dir=. -log_level=debug -id 1.1 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config.json &
     echo $! >> ${PID_FILE}
-    ./server -log_dir=. -log_level=debug -id 1.2 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config_3.json &
+    ./server -log_dir=. -log_level=debug -id 1.2 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config.json &
    echo $! >> ${PID_FILE}
-    ./server -log_dir=. -log_level=debug -id 1.3 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config_3.json &
+    ./server -log_dir=. -log_level=debug -id 1.3 -algorithm=iss -heartbeat=500 -batch=0 -segments=3 -segSize=32 -config=config.json &
     echo $! >> ${PID_FILE}
 else
     echo "Servers are already started in this folder."
